4616bc67cb0a0189959592f810d1da9086751542
thibautd2 <thibaut.betbeder@epita.fr>
2017-10-05 17:19:36 +0200

    Price gesture

--- src/application/Costumer.java
+++ src/application/Costumer.java
@@ -5,26 +5,57 @@ package application;
  */
 public class Costumer {
 
+
+    private float money;
+
+    public Costumer()
+    {
+        money = 0;
+    }
+
+    public Costumer(float money)
+    {
+        this.money = money;
+    }
+
+    public float getPrice(BEVARAGE bevarage)
+    {
+        float price = 0;
+        switch (bevarage)
+        {
+            case chocolate:
+                price = 0.5f;
+            break;
+            case cofee:
+                price = 0.6f;
+                break;
+            case tea:
+                price = 0.4f;
+        }
+        return price;
+    }
+
     public enum BEVARAGE {
         tea {
+            public float price = 0.4f;
             public String toString() {
                 return "T";
             }
         },
 
         cofee {
+            public Float price() {return 0.6f;  }
             public String toString() {
                 return "C";
             }
         },
         chocolate {
+            public Float price() {return 0.5f;  }
             public String toString() {
                 return "H";
             }
         }
     }
-
-
 
     public String order(BEVARAGE bevarage, Integer suggarAmmont)
     {
@@ -42,11 +73,31 @@ public class Costumer {
             stick = "0";
         }
 
+        float beveragePrice = getPrice(bevarage);
+       if(beveragePrice > money) {
+           float change = beveragePrice - money;
+           return show_Message("Need : " + change + "");
+       }
+       else {
+           money -= beveragePrice;
+       }
         order = order+":"+suggar+":"+stick;
         return order ;
     }
 
+    public String show_Message(String messageContent)
+    {
+        String message = "M:";
+        return message+messageContent;
+    }
 
 
+    public float getMoney() {
+        return money;
+    }
+
+    public void setMoney(float money) {
+        this.money = money;
+    }
 
 }
--- src/tests/CostumerTest.java
+++ src/tests/CostumerTest.java
@@ -9,32 +9,54 @@ import junit.framework.TestCase;
 public class CostumerTest extends TestCase {
 
     public void testOrder_tea() throws Exception {
-        Costumer costumer = new Costumer();
+        Costumer costumer = new Costumer(20);
 
         assertEquals("T::",costumer.order(Costumer.BEVARAGE.tea, 0));
         assertEquals("T::",costumer.order(Costumer.BEVARAGE.tea, -5));
     }
 
-    public void testOrder_tea_with_Suggar() throws Exception {
-        Costumer costumer = new Costumer();
-
-        assertEquals("T:1:0",costumer.order(Costumer.BEVARAGE.tea, 1));
-        assertEquals("T:2:0",costumer.order(Costumer.BEVARAGE.tea, 2));
-        assertEquals("T:2:0",costumer.order(Costumer.BEVARAGE.tea, 158));
-    }
-
-
     public void testOrder_cofee() throws Exception {
-        Costumer costumer = new Costumer();
+        Costumer costumer = new Costumer(20);
 
         assertEquals("C::",costumer.order(Costumer.BEVARAGE.cofee, 0));
     }
 
     public void testOrder_chocolate() throws Exception {
-        Costumer costumer = new Costumer();
+        Costumer costumer = new Costumer(20);
 
         assertEquals("H::",costumer.order(Costumer.BEVARAGE.chocolate, 0));
+    }
 
+    public void testOrder_with_Suggar() throws Exception {
+        Costumer costumer = new Costumer(20);
+
+        assertEquals("T:1:0",costumer.order(Costumer.BEVARAGE.tea, 1));
+        assertEquals("H:2:0",costumer.order(Costumer.BEVARAGE.chocolate, 2));
+        assertEquals("C:2:0",costumer.order(Costumer.BEVARAGE.cofee, 158));
+    }
+
+    public void testShow_Message()
+    {
+        Costumer costumer = new Costumer();
+
+        assertEquals("M:", costumer.show_Message(""));
+        String content = "Not enough money";
+        assertEquals("M:Not enough money", costumer.show_Message(content));
+
+    }
+
+    public void testNotEnougthMoney()
+    {
+        Costumer costumer = new Costumer();
+        assertEquals("M",costumer.order(Costumer.BEVARAGE.tea, 1).substring(0,1));
+    }
+
+    public void testBuyTea()
+    {
+        Costumer costumer = new Costumer(0.4f);
+        assertEquals("T::",costumer.order(Costumer.BEVARAGE.tea, 0));
+        assertEquals(costumer.getMoney(), 0f);
+        assertEquals("M",costumer.order(Costumer.BEVARAGE.tea, 1).substring(0,1));
     }
 
 }
